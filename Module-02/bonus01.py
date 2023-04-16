1840b036f027692d22720aece7a335f021b33c54
M4rrs <almasnorazman2803@outlook.com>
2023-04-16 17:54:13 +0800

    02 and 03 done

--- Module-02/bonus01.py
+++ Module-02/bonus01.py
@@ -1,2 +1,7 @@
 from datetime import datetime
+import pytz
 
+local_timezone = pytz.timezone("Asia/Kuala_Lumpur")
+localtime = datetime.now(pytz.utc).astimezone(local_timezone)
+
+print(localtime.time())
